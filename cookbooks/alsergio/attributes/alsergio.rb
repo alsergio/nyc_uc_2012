4929fc590d6f2097738233b5ac393709e81b91de
alsergio <alex.sergio@ezping.net>
2012-06-11 19:02:46 +0000

    Added the about_me cookbook

---
+++ cookbooks/alsergio/attributes/alsergio.rb
@@ -0,0 +1,4 @@
+set[:about_me][:first_name] = "Alex"
+default[:about_me][:favorite][:food] = "kabab"
+default[:about_me][:favorite][:drink] = "wodka"
+default[:about_me][:favorite][:hobby] = "tennis"
--- cookbooks/alsergio/metadata.rb
+++ cookbooks/alsergio/metadata.rb
@@ -4,4 +4,5 @@ license          "All rights reserved"
 description      "Installs/Configures alsergio"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.1"
+depends "rs_utils"
 recipe "alsergio::default","sends Hello World! to the logs"
