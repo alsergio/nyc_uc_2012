3fa3e8014b4135b279ae30ee517aa70037849a60
alsergio <alex.sergio@ezping.net>
2012-06-11 18:09:11 +0000

    Initial commit of alsergio cookbook

---
+++ cookbooks/alsergio/metadata.rb
@@ -0,0 +1,7 @@
+maintainer       "EPN"
+maintainer_email "YOUR_EMAIL"
+license          "All rights reserved"
+description      "Installs/Configures alsergio"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.0.1"
+recipe "alsergio::default","sends Hello World! to the logs"
